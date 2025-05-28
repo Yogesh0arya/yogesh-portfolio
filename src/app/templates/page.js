9dc082d69e6b82cb214fb819ae7d24fced23073f
Yogesh Arya <yogesh084arya@gmail.com>
2025-05-28 13:57:22 +0530

    fix template cards

--- src/app/templates/page.js
+++ src/app/templates/page.js
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -20,32 +21,40 @@ function Templates() {
         {templates.map((template) => (
           <div
             key={template.id}
-            className="hover:scale-110 transition-all hover:shadow-md relative group"
+            className="shadow-md rounded-lg overflow-hidden bg-black"
           >
-            <div className="absolute hidden p-2 group-hover:flex justify-between w-full text-white bg-black/80">
-              <h1>{template.name}</h1>
-              <p>Price ${template.price}</p>
-            </div>
             <Image
               src={template.image}
               width={200}
               height={200}
               alt="logo"
-              className="w-full"
+              className="w-full p-2 hover:scale-105 transition-all"
             />
-            <Link
-              href={`https://api.whatsapp.com/send/?phone=918770852087&text=Hey Yogesh,+I+want+to+buy+${template.name}+template+at+$+${template.price}&type=phone_number&app_absent=0 `}
-              className="absolute hidden group-hover:inline-block bottom-0  text-white cursor-pointer m-2 px-3 py-1 bg-[#FF3333] rounded-full text-sm"
-            >
-              Buy
-            </Link>
 
-            <Link
-              href={template.link}
-              className="absolute hidden group-hover:inline-block bottom-0 right-0  text-black cursor-pointer m-2 px-3 py-1 bg-white rounded-full text-sm"
-            >
-              Demo
-            </Link>
+            <div className="p-2 rounded-lg bg-white dark:bg-gray-800">
+              <h1 className="text-2xl">{template.name}</h1>
+              <Link
+                className="py-1 px-3 bg-black dark:bg-white dark:text-black  rounded-full text-white text-sm"
+                href={template.link}
+              >
+                DEMO
+              </Link>
+              <div className="grid grid-cols-3 justify-between items-center mt-6">
+                <div>
+                  <p className="text-sm">PRICE</p>
+                  <p className="text-black dark:text-white font-bold">
+                    ${template.price}
+                  </p>
+                </div>
+
+                <Link
+                  className="col-span-2 text-center p-2 bg-green-700 rounded-sm text-white text-sm"
+                  href={`https://api.whatsapp.com/send/?phone=918770852087&text=Hey Yogesh,+I+want+to+buy+${template.name}+template+at+$+${template.price}&type=phone_number&app_absent=0 `}
+                >
+                  BUY
+                </Link>
+              </div>
+            </div>
           </div>
         ))}
       </div>
